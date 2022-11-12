e67d5d4afe0591153efcea2a25e7dbbb11e68567
nhantran3395 <thanhnhan33.95@gmail.com>
2022-11-12 10:25:39 +0700

    remove .idea from catch. remove default styling. add tremor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,15 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>ESF Global Search</title>
         <meta name="description" content="Demo of ESF Global Search feature" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <h1>Hello world</h1>
       </main>
     </div>
