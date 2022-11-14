8d4e776964b505e57ac487716dd15dd6ef1a710e
nhantran3395 <thanhnhan33.95@gmail.com>
2022-11-14 20:23:19 +0700

    add prettier

--- components/home-page-detail/index.tsx
+++ components/home-page-detail/index.tsx
@@ -1,11 +1,11 @@
 import { Block, Card } from '@tremor/react';
 
-export default function HomePageDetail () {
-    return (
-        <Block marginTop="mt-6">
-            <Card>
-                <div className="h-96" />
-            </Card>
-        </Block>
-    );
+export default function HomePageDetail() {
+  return (
+    <Block marginTop="mt-6">
+      <Card>
+        <div className="h-96" />
+      </Card>
+    </Block>
+  );
 }
--- components/home-page-overview/index.tsx
+++ components/home-page-overview/index.tsx
@@ -1,28 +1,34 @@
 import { Block, Card, ColGrid } from '@tremor/react';
 
-export default function  HomePageOverview () {
-    return (
-        <>
-            <Block marginTop="mt-6">
-                <Card>
-                    <div className="h-80" />
-                </Card>
-            </Block>
+export default function HomePageOverview() {
+  return (
+    <>
+      <Block marginTop="mt-6">
+        <Card>
+          <div className="h-80" />
+        </Card>
+      </Block>
 
-            <ColGrid numColsMd={ 2 } numColsLg={ 3 } gapX="gap-x-6" gapY="gap-y-6" marginTop="mt-6">
-                <Card>
-                    { /* Placeholder to set height */ }
-                    <div className="h-28" />
-                </Card>
-                <Card>
-                    { /* Placeholder to set height */ }
-                    <div className="h-28" />
-                </Card>
-                <Card>
-                    { /* Placeholder to set height */ }
-                    <div className="h-28" />
-                </Card>
-            </ColGrid>
-        </>
-    );
+      <ColGrid
+        numColsMd={2}
+        numColsLg={3}
+        gapX="gap-x-6"
+        gapY="gap-y-6"
+        marginTop="mt-6"
+      >
+        <Card>
+          {/* Placeholder to set height */}
+          <div className="h-28" />
+        </Card>
+        <Card>
+          {/* Placeholder to set height */}
+          <div className="h-28" />
+        </Card>
+        <Card>
+          {/* Placeholder to set height */}
+          <div className="h-28" />
+        </Card>
+      </ColGrid>
+    </>
+  );
 }
