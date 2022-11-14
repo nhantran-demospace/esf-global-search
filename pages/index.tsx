7dcea35a1b2a02a3c86410ed6f34b96551747f09
nhantran3395 <thanhnhan33.95@gmail.com>
2022-11-14 20:22:06 +0700

    add prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,21 @@
 import Head from 'next/head';
 import { useState } from 'react';
-import {
-    Tab,
-    TabList
-} from '@tremor/react';
+import { Tab, TabList } from '@tremor/react';
 
 import HomePageOverview from 'components/home-page-overview';
 import HomePageDetail from 'components/home-page-detail';
 
 enum HomePageView {
-    'Overview' = 'Overview',
-    'Detail' = 'Detail'
+  'Overview' = 'Overview',
+  'Detail' = 'Detail'
 }
 
 export default function Home() {
-    const [selectedView, setSelectedView] = useState<HomePageView>(HomePageView.Overview);
+  const [selectedView, setSelectedView] = useState<HomePageView>(
+    HomePageView.Overview
+  );
 
-    return (
+  return (
     <div>
       <Head>
         <title>ESF Global Search</title>
@@ -24,13 +23,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        <main className="bg-slate-50 p-6 sm:p-10">
-            <TabList defaultValue={ HomePageView.Overview } handleSelect={ (value) => setSelectedView(value) } marginTop="mt-6">
-                <Tab value={ HomePageView.Overview } text={ HomePageView.Overview } />
-                <Tab value={ HomePageView.Detail } text={ HomePageView.Detail } />
-            </TabList>
-            { selectedView === HomePageView.Overview ? <HomePageOverview/> : <HomePageDetail/> }
-        </main>
+      <main className="bg-slate-50 p-6 sm:p-10">
+        <TabList
+          defaultValue={HomePageView.Overview}
+          handleSelect={(value) => setSelectedView(value)}
+          marginTop="mt-6"
+        >
+          <Tab value={HomePageView.Overview} text={HomePageView.Overview} />
+          <Tab value={HomePageView.Detail} text={HomePageView.Detail} />
+        </TabList>
+        {selectedView === HomePageView.Overview ? (
+          <HomePageOverview />
+        ) : (
+          <HomePageDetail />
+        )}
+      </main>
     </div>
-    );
+  );
 }
