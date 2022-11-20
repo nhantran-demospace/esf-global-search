7de0a0115b5c14524e4b698715dd29f9a89459b8
nhantran3395 <thanhnhan33.95@gmail.com>
2022-11-20 11:16:28 +0700

    mock table in level 1 detail card

--- components/home/overview-view/index.tsx
+++ components/home/overview-view/index.tsx
@@ -1,20 +1,28 @@
 import { ColGrid } from '@tremor/react';
 
+import { selectSelectedLevel0Id } from 'slices/location.slice';
+import { useAppSelector } from 'hooks';
+
 import Level0SummaryCard from 'components/home/overview-view/level0-summary-card';
 import Level1DetailCard from 'components/home/overview-view/level1-detail-card';
+import { getLevel1LocationsMatchedWithLevel0 } from '../../../helpers/location.helper';
 
 export default function HomePageOverview() {
+  const selectedLevel0Id = useAppSelector(selectSelectedLevel0Id);
+  const level1Locations = selectedLevel0Id
+    ? getLevel1LocationsMatchedWithLevel0(selectedLevel0Id)
+    : [];
+
   return (
     <>
       <Level0SummaryCard />
-
       <ColGrid numColsMd={3} gapX="gap-x-6" gapY="gap-y-6" marginTop="mt-6">
-        <Level1DetailCard />
-        <Level1DetailCard />
-        <Level1DetailCard />
-        <Level1DetailCard />
-        <Level1DetailCard />
-        <Level1DetailCard />
+        {level1Locations.map((location) => (
+          <Level1DetailCard
+            key={`${location.locationId}-${location.locationName}`}
+            location={location}
+          />
+        ))}
       </ColGrid>
     </>
   );
--- components/home/overview-view/level1-detail-card.tsx
+++ components/home/overview-view/level1-detail-card.tsx
@@ -1,10 +1,166 @@
-import { Title, Footer, ButtonInline, Card } from '@tremor/react';
+import {
+  Title,
+  Footer,
+  ButtonInline,
+  Card,
+  DeltaType,
+  MultiSelectBox,
+  MultiSelectBoxItem,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableHeaderCell,
+  Flex
+} from '@tremor/react';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 
-export default function Example() {
+import { Location } from 'models/location.model';
+import { useState } from 'react';
+
+interface level1DetailCardProps {
+  location: Location;
+}
+
+type SalesPerson = {
+  name: string;
+  leads: number;
+  sales: string;
+  quota: string;
+  variance: string;
+  region: string;
+  status: string;
+  deltaType: DeltaType;
+};
+
+const salesPeople: SalesPerson[] = [
+  {
+    name: 'Peter Doe',
+    leads: 45,
+    sales: '1,000,000',
+    quota: '1,200,000',
+    variance: 'low',
+    region: 'Region A',
+    status: 'overperforming',
+    deltaType: 'moderateIncrease'
+  },
+  {
+    name: 'Lena Whitehouse',
+    leads: 35,
+    sales: '900,000',
+    quota: '1,000,000',
+    variance: 'low',
+    region: 'Region B',
+    status: 'average',
+    deltaType: 'unchanged'
+  },
+  {
+    name: 'Phil Less',
+    leads: 52,
+    sales: '930,000',
+    quota: '1,000,000',
+    variance: 'medium',
+    region: 'Region C',
+    status: 'underperforming',
+    deltaType: 'moderateDecrease'
+  },
+  {
+    name: 'John Camper',
+    leads: 22,
+    sales: '390,000',
+    quota: '250,000',
+    variance: 'low',
+    region: 'Region A',
+    status: 'overperforming',
+    deltaType: 'increase'
+  },
+  {
+    name: 'Max Balmoore',
+    leads: 49,
+    sales: '860,000',
+    quota: '750,000',
+    variance: 'low',
+    region: 'Region B',
+    status: 'overperforming',
+    deltaType: 'increase'
+  },
+  {
+    name: 'Peter Moore',
+    leads: 82,
+    sales: '1,460,000',
+    quota: '1,500,000',
+    variance: 'low',
+    region: 'Region A',
+    status: 'average',
+    deltaType: 'unchanged'
+  },
+  {
+    name: 'Joe Sachs',
+    leads: 49,
+    sales: '1,230,000',
+    quota: '1,800,000',
+    variance: 'medium',
+    region: 'Region B',
+    status: 'underperforming',
+    deltaType: 'moderateDecrease'
+  }
+];
+
+export default function Level1DetailCard({
+  location: { locationName }
+}: level1DetailCardProps) {
+  const [selectedNames, setSelectedNames] = useState<string[]>([]);
+
+  const isSalesPersonSelected = (salesPerson: SalesPerson) =>
+    selectedNames.includes(salesPerson.name) || selectedNames.length === 0;
+
   return (
     <Card>
-      <Title>Level 1</Title>
+      <Flex justifyContent={'justify-between'}>
+        <Title>{locationName}</Title>
+        <MultiSelectBox
+          handleSelect={(value) => setSelectedNames(value)}
+          placeholder="Select Level 2"
+          maxWidth="max-w-xs"
+        >
+          {salesPeople.map((item) => (
+            <MultiSelectBoxItem
+              key={item.name}
+              value={item.name}
+              text={item.name}
+            />
+          ))}
+        </MultiSelectBox>
+      </Flex>
+      <Table marginTop="mt-4">
+        <TableHead>
+          <TableRow>
+            <TableHeaderCell>Level 2</TableHeaderCell>
+            <TableHeaderCell textAlignment="text-right">Open</TableHeaderCell>
+            <TableHeaderCell textAlignment="text-right">
+              Pending Update
+            </TableHeaderCell>
+            <TableHeaderCell textAlignment="text-right">
+              Void Pending Actions
+            </TableHeaderCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {salesPeople
+            .filter((item) => isSalesPersonSelected(item))
+            .map((item) => (
+              <TableRow key={item.name}>
+                <TableCell>{item.name}</TableCell>
+                <TableCell textAlignment="text-right">{item.leads}</TableCell>
+                <TableCell textAlignment="text-right">{item.sales}</TableCell>
+                <TableCell textAlignment="text-right">{item.quota}</TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+
       <Footer>
         <ButtonInline
           size="sm"
